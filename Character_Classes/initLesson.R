2a5652556d698140521e26c5e549cfb926f0a988
Jon Calder <jonmcalder@gmail.com>
2016-09-16 15:31:35 +0200

    Update one of the hints in the Anchors lesson and get started on the Character_Classes lesson

--- Character_Classes/initLesson.R
+++ Character_Classes/initLesson.R
@@ -2,3 +2,13 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
+
+
+create_password <- function(length){
+  paste(sample(c(0:9, letters, LETTERS), length, replace=TRUE), collapse="")
+}
+
+passwords = sapply(rep(4, 10), FUN = create_password)
+
+
+
