ed3744200cfab876003ac13b43907d368bfd2b2e
jonmcalder <jonmcalder@gmail.com>
2017-06-02 01:21:56 +0200

    Set lesson silently

--- run_tests.R
+++ run_tests.R
@@ -2,7 +2,7 @@ library(swirlify)
 library(swirl)
 
 # Test course
-set_lesson("Introduction/lesson.yaml")
+set_lesson("Introduction/lesson.yaml", open_lesson = FALSE, silent = TRUE)
 test_course()
 
 # Check that course can be installed
